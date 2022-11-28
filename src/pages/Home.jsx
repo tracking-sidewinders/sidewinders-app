e7103f3b39791d2ec4813cebb34a456ae0091145
Patima Poochai <patimapoochai@gmail.com>
2022-11-28 13:56:42 -1000

    God this works now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../App.css';
-  
+
+//TODO add progress bars to the dashboard
+// fetch training completion data
+// render it to website
 function Home() {
     return ( 
       <h1> Hi I am DASHBOARD/HOME </h1>
